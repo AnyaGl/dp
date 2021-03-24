4cba263b1516a84b0948c116bd6aa1ad864d9589
AnyaGl <gladysheva_2000@inbox.ru>
2021-03-24 11:35:07 +0300

    [pa4] Добавлена сборка проектов в скриптах запуска приложения. Добавлен отдельный скрипт, выполняющий сборку проектов.

--- scripts/start.cmd
+++ scripts/start.cmd
@@ -3,13 +3,19 @@ SET PATH_TO_NGINX=%~dp0..\nginx\
 SET PATH_TO_RANK_CALCULATOR=%~dp0..\RankCalculator\
 SET PATH_TO_EVENTS_LOGGER=%~dp0..\EventsLogger\
 
+cd %PATH_TO_APP%
+dotnet build
 start /d %PATH_TO_APP% dotnet run --no-build --urls "http://localhost:5001"
 start /d %PATH_TO_APP% dotnet run --no-build --urls "http://localhost:5002"
 
-start /d %PATH_TO_RANK_CALCULATOR% dotnet run
-start /d %PATH_TO_RANK_CALCULATOR% dotnet run
+cd %PATH_TO_RANK_CALCULATOR%
+dotnet build
+start /d %PATH_TO_RANK_CALCULATOR% dotnet run --no-build
+start /d %PATH_TO_RANK_CALCULATOR% dotnet run --no-build
 
-start /d %PATH_TO_EVENTS_LOGGER% dotnet run
-start /d %PATH_TO_EVENTS_LOGGER% dotnet run
+cd %PATH_TO_EVENTS_LOGGER%
+dotnet build
+start /d %PATH_TO_EVENTS_LOGGER% dotnet run --no-build
+start /d %PATH_TO_EVENTS_LOGGER% dotnet run --no-build
 
 start /d %PATH_TO_NGINX% nginx.exe
